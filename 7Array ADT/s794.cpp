361213617eed152d820bdac5b8c023cfaad073c0
dnyaneshwar-lohar <dnyneshwarlohar@gmail.com>
2023-08-16 23:49:16 +0530

    Learnt new concept in Arr Adt and created functions

--- 7Array ADT/s794.cpp
+++ 7Array ADT/s794.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std;
@@ -149,6 +149,66 @@ int RBinSearch(int a[],int l, int h, int key)
     return -1;
 } 
 
+int Get(struct Array *arr,int index)
+{
+    if(index>=0 && index<arr->length)
+    {
+        return(arr->A[index]);
+    }
+    return -1;
+}
+
+void Set(struct Array *arr,int index,int x)
+{
+    if(index >= 0 && index < arr->length)
+    {
+        arr->A[index]=x;
+    }
+}
+
+int Max(struct Array *arr)
+{
+    int max=arr->A[0];
+    for (int i = 1; i < arr->length; i++)
+    {
+        if(arr->A[i]>max)
+        {
+            max=arr->A[i];
+        }
+    }
+    return max;
+}
+
+int Min(struct Array *arr)
+{
+    int min=arr->A[0];
+    for (int i = 1; i < arr->length; i++)
+    {
+        if(arr->A[i]<min)
+        {
+            min=arr->A[i];
+        }
+    }
+    return min;
+}
+
+int Sum(struct Array *arr)
+{
+    int Total=0;
+    for (int i = 0; i < arr->length; i++)
+    {
+        Total+=arr->A[i];
+    }
+    return Total;
+}
+
+int RSum(struct Array *arr,int n)
+{
+    if(n<0)
+        return 0;
+    else
+        return RSum(arr,n-1) + arr->A[n];
+}
 int main()
 {
     struct Array arr = {{4, 8, 9, 3, 5},20,5};
@@ -159,8 +219,17 @@ int main()
     // printf("%d\n",LinearSearch(arr,9));
     // printf("%d\n",LinearSearch2(&arr,9));
     // printf("%d\n",LinearSearch3(&arr,9));
-    printf("%d\n",BinarySearch(arr,0));
-    printf("%d\n",RBinSearch(arr.A,0,arr.length,0));
+    printf("%d\n",BinarySearch(arr,4));
+    printf("%d\n",RBinSearch(arr.A,0,arr.length,4));
+
+    printf("%d\n",Get(&arr, 4));
+    Set(&arr,4,12);
+
+    printf("%d\n",Max(&arr));
+    printf("%d\n",Min(&arr));
+
+    printf("%d\n",Sum(&arr));
+    printf("%d\n",RSum(&arr,arr.length-1));
 
     Display(arr);
 
